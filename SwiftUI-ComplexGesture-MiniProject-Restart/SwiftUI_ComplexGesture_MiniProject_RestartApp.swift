785fee21970929989c22eb815384093e9ac03d3b
ekayaint <ekayaint@gmail.com>
2023-05-09 20:57:52 +0300

    Initial Commit

---
+++ SwiftUI-ComplexGesture-MiniProject-Restart/SwiftUI_ComplexGesture_MiniProject_RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ComplexGesture_MiniProject_RestartApp.swift
+//  SwiftUI-ComplexGesture-MiniProject-Restart
+//
+//  Created by ekayaint on 9.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ComplexGesture_MiniProject_RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
